fa387010d4d8d1fdf8346a7a4e9b184b0184625b
Freshers Factory <contact.freshersfactory@gmail.com>
2024-12-05 13:08:56 +0530

    new html css js files added

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can add interactivity here if needed in the future
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Freshers\' Factory website is under construction!');
+});
